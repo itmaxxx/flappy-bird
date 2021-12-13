af5472d436c5a062db2568e14fe2f14fea1f96e1
itmaxxx <dmitrievmaks77@gmail.com>
2021-12-13 21:38:10 +0200

    UI interactions, play, game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+	public Player player;
+	public Text scoreText;
+	public GameObject playButton;
+	public GameObject gameOver;
     private int score;
+
+	private void Awake()
+	{
+		gameOver.SetActive(false);
+	
+		Application.targetFrameRate = 60;
+
+		Pause();
+	}
+
+	public void Play()
+	{
+		score = 0;
+		scoreText.text = score.ToString();
+
+		gameOver.SetActive(false);
+		playButton.SetActive(false);
+
+		Time.timeScale = 1f;
+
+		player.enabled = true;
+
+		Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+		for (int i = 0; i < pipes.Length; i++)
+		{
+			Destroy(pipes[i].gameObject);
+		}
+	}
+
+	public void Pause()
+	{
+		Time.timeScale = 0f;
+		player.enabled = false;
+	}
 
 	public void GameOver()
 	{
-		Debug.Log("Game Over");
+		gameOver.SetActive(true);
+		playButton.SetActive(true);
+
+		Pause();
 	}
 
     public void IncreaseScore()
 	{
-		Debug.Log("Score");
 		score += 1;
+
+		scoreText.text = score.ToString();
 	}
 }
